90c790bde16823992bbc5ee5403556983beac2d7
Pau Marro <pau.marro@code.berlin>
2022-04-26 09:50:39 +0200

    Merged heads

---
+++ migrations/versions/a38f11a6bd12_merging_heads.py
@@ -0,0 +1,24 @@
+"""Merging heads
+
+Revision ID: a38f11a6bd12
+Revises: c79f5259b06b, f20e82440262
+Create Date: 2022-04-26 09:49:29.497064
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a38f11a6bd12'
+down_revision = ('c79f5259b06b', 'f20e82440262')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
